a84ccbd530c72edb0dffa5e5de1b0b4ce2869188
jeffhowarth <jhowarth@middlebury.edu>
2023-05-08 19:56:53 -0400

    mce roi mask

--- solutions/_06_mce_restore.py
+++ solutions/_06_mce_restore.py
@@ -22,31 +22,32 @@ wbt = WhiteboxTools()
 #  Working directories
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Define personal storage root.
-# This is the path where you will store inputs and outputs from this workflow.
-# For example, my root points to the directory (folder) of s23 in GEOG0310 
+# Define personal storage root: a path where to store inputs and outputs.
+# In this example, the root points to a directory (folder) named s23 in GEOG0310 
 # on an external drive named drosera. 
 
 root = "/Volumes/drosera/GEOG0310/s23"
+
+# These are the other folders referenced by the workflow. 
 
 inputs = root+"/inputs/"
 keeps = root+"/keeps/"
 mce = root+"/mce/"
 
-# Define MCE as the primary working directory.
+# Define mce as the primary working directory.
 # The weighted overlay tool takes a list of input files as a string. This does not
-# play nicely with the scheme I have been used to distinguish different directories
-# as variables. So I added the step below the uses the 'mce' folder as the working directory
-# for all layers generated in this workflow. 
+# play nicely with the scheme that I have been using to distinguish different directories
+# as variables. So I added the step below to define the 'mce' folder as the place to put
+# all layers generated in this workflow. 
 
 wbt.work_dir = mce
 
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#  Required datasets:
+#  Required datasets
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Point to directory where you hold input data. 
-# All but the last one are available on the public Google starter drive.
+# This stores inputs as variables to make updates easier in the future. 
+# All but the last one are currently available on the public Google starter drive.
 # The last layer gets kicked out by the _04_select_valley_corridor.py script. 
 
 lc = inputs+"LCHP_1m_midd.tif" 
@@ -107,7 +108,7 @@ wbt.resample(
 wbt.reclass(
     i = "_0601_lc_mama.tif", 
     output = "_0612_lc_suitability.tif", 
-    reclass_vals = "0;1;10;2;0;3;5;4;10;5;0;6;0;7;0;8;0;9;0;10", 
+    reclass_vals = "2;1;2;2;0;3;1;4;2;5;0;6;0;7;0;8;0;9;0;10", 
     assign_mode=True
 )
 
@@ -140,4 +141,33 @@ wbt.weighted_overlay(
     cost = 'false;true;false', 
     constraints = "_0604_corridors_resample.tif", 
     scale_max = 100.0,
+)
+
+# ------------------------------------------------------------------------------
+# Mask land outside of the region of interest (Town of Middlebury)
+# ------------------------------------------------------------------------------
+
+# Make binary of land in region of interest. 
+
+wbt.not_equal_to(
+    input1 = "_0603_nc_resample.tif", 
+    input2 = 0, 
+    output = "_0631_roi.tif"
+)
+
+# Mask locations outside of roi.
+
+wbt.set_nodata_value(
+    i = "_0631_roi.tif", 
+    output = "_0632_roi_mask.tif", 
+    back_value=0.0, 
+)
+
+
+# Apply mask to mce results.
+
+wbt.multiply(
+    input1 = "_0632_roi_mask.tif", 
+    input2 = '_0623_mce_result.tif', 
+    output = "_0633_mce_results_in_roi.tif", 
 )
